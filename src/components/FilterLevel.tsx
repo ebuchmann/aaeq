ed1b5196d107be1269a8017684ffab0b9441293c
Eric Buchmann <ericbuchmann20@gmail.com>
2025-10-15 06:02:15 -0700

    add labels to filters

--- src/components/FilterLevel.tsx
+++ src/components/FilterLevel.tsx
@@ -3,6 +3,7 @@ import { filtersAtom } from '@/state/filters'
 import { Popover } from '@radix-ui/react-popover'
 import { PopoverContent, PopoverTrigger } from './ui/popover'
 import { Input } from './ui/input'
+import { Label } from './ui/label'
 
 export const FilterLevel = () => {
   const setFilters = useSetAtom(filtersAtom)
@@ -12,30 +13,42 @@ export const FilterLevel = () => {
     <Popover>
       <PopoverTrigger>Level</PopoverTrigger>
       <PopoverContent className="flex flex-col gap-2">
-        <Input
-          type="number"
-          placeholder="Min Class Level"
-          value={minClassLevel === 0 ? '' : minClassLevel}
-          onChange={(e) => setFilters((prev) => ({ ...prev, minClassLevel: Number(e.target.value) }))}
-        />
-        <Input
-          type="number"
-          placeholder="Max Class Level"
-          value={maxClassLevel === 0 ? '' : maxClassLevel}
-          onChange={(e) => setFilters((prev) => ({ ...prev, maxClassLevel: Number(e.target.value) }))}
-        />
-        <Input
-          type="number"
-          placeholder="Min Total Level"
-          value={minTotalLevel === 0 ? '' : minTotalLevel}
-          onChange={(e) => setFilters((prev) => ({ ...prev, minTotalLevel: Number(e.target.value) }))}
-        />
-        <Input
-          type="number"
-          placeholder="Max Total Level"
-          value={maxTotalLevel === 0 ? '' : maxTotalLevel}
-          onChange={(e) => setFilters((prev) => ({ ...prev, maxTotalLevel: Number(e.target.value) }))}
-        />
+        <Label>
+          Min Class Level
+          <Input
+            type="number"
+            placeholder="Min Class Level"
+            value={minClassLevel === 0 ? '' : minClassLevel}
+            onChange={(e) => setFilters((prev) => ({ ...prev, minClassLevel: Number(e.target.value) }))}
+          />
+        </Label>
+        <Label>
+          Max Class Level
+          <Input
+            type="number"
+            placeholder="Max Class Level"
+            value={maxClassLevel === 0 ? '' : maxClassLevel}
+            onChange={(e) => setFilters((prev) => ({ ...prev, maxClassLevel: Number(e.target.value) }))}
+          />
+        </Label>
+        <Label>
+          Min Total Level
+          <Input
+            type="number"
+            placeholder="Min Total Level"
+            value={minTotalLevel === 0 ? '' : minTotalLevel}
+            onChange={(e) => setFilters((prev) => ({ ...prev, minTotalLevel: Number(e.target.value) }))}
+          />
+        </Label>
+        <Label>
+          Max Total Level
+          <Input
+            type="number"
+            placeholder="Max Total Level"
+            value={maxTotalLevel === 0 ? '' : maxTotalLevel}
+            onChange={(e) => setFilters((prev) => ({ ...prev, maxTotalLevel: Number(e.target.value) }))}
+          />
+        </Label>
       </PopoverContent>
     </Popover>
   )
